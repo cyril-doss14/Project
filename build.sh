cf73f9b8cb061039b6e5c210e7c64f86a61eca15
cyril-doss14 <cyrildoss007@gmail.com>
2024-08-07 18:44:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Build the Docker image
-docker build -t my-image1 .
+docker build -t myimage .
 
-
+# Check if the build was successful
 if [ $? -eq 0 ]; then
   echo "Docker image built successfully."
 else
   echo "Docker image build failed."
   exit 1
 fi
-
